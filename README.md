1b9bbec40fefd140df4955da704c3e2fb639b35b
toshihisa <i.toshihisa@gmail.com>
2016-11-05 16:13:24 +0900

    1st commit

---
+++ README.md
@@ -0,0 +1,103 @@
+
+# Dependency
+- aws cli
+
+Via __requirements.txt__
+- netaddr
+- sh
+
+`direnv` Recomend!
+
+# Getting start
+## 0. Install
+`pip install -r requirements.txt`
+
+## 1. Create ip-set
+If you have already created ip-set, skip this step.
+
+```
+$ aws waf get-change-token
+{
+    "ChangeToken": "xxxxxxxxx-c130-4dbf-839c-yyyyyyyyy"
+}
+
+$ aws waf create-ip-set --name hoge-ip --change-token xxxxxxxxx-c130-4dbf-839c-yyyyyyyyy
+{
+    "IPSet": {
+        "IPSetId": "vvvvvvvv-ebef-417c-b788-wwwwwwwwww",
+        "Name": "hoge-ip",
+        "IPSetDescriptors": []
+    },
+    "ChangeToken": "xxxxxxxxx-c130-4dbf-839c-yyyyyyyyy"
+}
+
+```
+
+## 2. Get `hoge-ip`'s ip-set
+```
+$ aws waf get-ip-set --ip-set-id vvvvvvvv-ebef-417c-b788-wwwwwwwwww > hoge-ip.json
+```
+
+## 3. Edit `hoge-ip.json`
+CIDR OK!
+
+```
+{
+  "IPSet": {
+    "IPSetId": "vvvvvvvv-ebef-417c-b788-wwwwwwwwww", 
+    "Name": "hoge-ip", 
+    "IPSetDescriptors": [
+      {
+        "Type": "IPV4",
+        "Value": "192.168.1.0/28"
+      }
+    ]
+  }
+}
+```
+
+## 4. Convert `hoge-ip.json` to Classed Subnet
+
+Confirm diff.
+
+```
+./conv-cidr-changeset.py -d < hoge-ip.json
+Append:
+['192.168.1.5/32',
+ '192.168.1.8/32',
+ '192.168.1.12/32',
+ '192.168.1.15/32',
+ '192.168.1.7/32',
+ '192.168.1.10/32',
+ '192.168.1.6/32',
+ '192.168.1.1/32',
+ '192.168.1.4/32',
+ '192.168.1.11/32',
+ '192.168.1.13/32',
+ '192.168.1.9/32',
+ '192.168.1.14/32',
+ '192.168.1.2/32',
+ '192.168.1.0/32',
+ '192.168.1.3/32']
+Revoke:
+[]
+```
+
+To convert.
+```
+./conv-cidr-changeset.py -d < hoge-ip.json > conved_hoge-ip.json
+```
+
+## 5. Apply
+```
+aws waf update-ip-set --cli-input-json file://converted_hoge-ip.json
+```
+
+# Features
+- Convert CIDR to Classed subnet. (INSERT & DELETE)
+- Diff current ip-set with local-json-file.
+- /22 -> /24 * 2, /28 -> /32 * 16
+- get-change-token & set it converted-json file
+
+# ToDo
+- IPv6
---
+++ conv-cidr-changeset.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+import sys
+import pprint
+from netaddr import *
+from sh import aws
+
+
+def _get_curr_ipset(ipset_id):
+    j = json.loads(
+        aws(
+            "waf",
+            "get-ip-set",
+            "--ip-set-id",
+            ipset_id,
+            "--output",
+            "json"
+        ).stdout
+    )
+    return [c['Value'] for c in j['IPSet']['IPSetDescriptors']]
+
+
+def _get_new_ipset(j):
+    newl = []
+    for l in j['IPSet']['IPSetDescriptors']:
+        ip = IPNetwork(l['Value'])
+        if not (ip.prefixlen % 8):
+            newl.append("%s" % ip.cidr)
+        else:
+            bit = (ip.prefixlen / 8 + 1) * 8
+            for s_ip in ip.subnet(bit):
+                newl.append("%s" % (s_ip))
+
+    return newl
+
+
+def _get_diff(curr, newl):
+    return (
+        list(set(curr) - set(newl)),
+        list(set(newl) - set(curr))
+        )
+
+
+def main():
+
+    diff_only = False
+    if len(sys.argv) > 1 and sys.argv[1] in ("-d", "--diff"):
+        diff_only = True
+
+    src = json.load(sys.stdin, encoding='utf-8')
+    # get current ipset
+    newl = _get_new_ipset(src)
+    curr = _get_curr_ipset(src['IPSet']['IPSetId'])
+    (del_list, add_list) = _get_diff(curr, newl)
+
+    if diff_only:
+        print "Append:"
+        pprint.pprint(add_list)
+        print "Revoke:"
+        pprint.pprint(del_list)
+        exit(0)
+
+    token = aws(
+        "waf",
+        "get-change-token",
+        "--output",
+        "text"
+        ).stdout
+
+    ipset_list = {
+        "IPSetId": src['IPSet']['IPSetId'],
+        "ChangeToken": token.rstrip(),
+        'Updates': []
+        }
+
+    for l in del_list:
+        ipset_list['Updates'].append(
+            {
+                "Action": "DELETE",
+                "IPSetDescriptor": {
+                    "Type": "IPV4",
+                    "Value": "%s" % l
+                    }
+            })
+
+    for l in add_list:
+        ipset_list['Updates'].append(
+            {
+                "Action": "INSERT",
+                "IPSetDescriptor": {
+                    "Type": "IPV4",
+                    "Value": "%s" % l
+                    }
+            })
+
+    print json.dumps(ipset_list, indent=4)
+
+if __name__ == '__main__':
+    main()
